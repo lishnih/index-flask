0a82055b9c71ce8ae686561c53b1e3451c2e68ee
Stan Ovchinnikov <lishnih@gmail.com>
2016-05-25 10:13:56 +0300

    Append standalone.py.

---
+++ standalone.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Stan 2016-04-24
+
+from __future__ import ( division, absolute_import,
+                         print_function, unicode_literals )
+
+import logging
+
+from index import app
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
+    app.run(debug=True)
